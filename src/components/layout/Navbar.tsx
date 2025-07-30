07de676f50809e06708a3d0ef895a95c7935e78a
md-nasim-mondal <nasim.programmer24@gmail.com>
2025-07-31 01:32:54 +0600

    implement thme toggle system with shadcdn

--- src/components/layout/Navbar.tsx
+++ src/components/layout/Navbar.tsx
@@ -1,15 +1,19 @@
 import Logo from "@/assets/Logo";
 import { Link } from "react-router";
+import { ModeToggle } from "../mode-toggle";
 
 const Navbar = () => {
   return (
-    <nav className='max-w-7xl mx-auto h-16 flex items-center gap-3 px-5'>
+    <nav className='max-w-7xl mx-auto h-16 flex justify-between items-center gap-3 px-5'>
       <div className='flex items-center'>
         <Logo />
         <span className='font-bold ml-2'>Task</span>
       </div>
-      <Link to="/">Tasks</Link>
-      <Link to="/users">Users</Link>
+        <Link to='/'>Tasks</Link>
+        <Link to='/users'>Users</Link>
+      <div className="ml-auto">
+        <ModeToggle />
+      </div>
     </nav>
   );
 };
