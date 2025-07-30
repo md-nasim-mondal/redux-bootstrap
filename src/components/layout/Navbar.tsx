1f841fe971ecd6e6b290f366120c3f5351a07241
md-nasim-mondal <nasim.programmer24@gmail.com>
2025-07-31 00:21:28 +0600

    project initialized

---
+++ src/components/layout/Navbar.tsx
@@ -0,0 +1,14 @@
+import Logo from "@/assets/Logo";
+
+const Navbar = () => {
+  return (
+    <nav className='max-w-7xl mx-auto h-16 flex items-center gap-3 px-5'>
+      <div className='flex items-center'>
+        <Logo />
+        <span className='font-bold ml-2'>Task</span>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
---
+++ src/redux/middlewares/logger.ts
@@ -0,0 +1,10 @@
+const logger = (state) => (next) => (action) => {
+  console.group(action.type);
+  console.info("Previous State",state.getState());
+  const result = next(action);
+  console.info("Next State", state.getState());
+  console.groupEnd();
+  return result;
+};
+
+export default logger;
