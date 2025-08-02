fb8ae33e65d41c849f27b94ef90258d054312505
md-nasim-mondal <nasim.programmer24@gmail.com>
2025-08-02 18:35:59 +0600

    implement useGetTasksQuery in Tasks page with redux

--- src/pages/Tasks.tsx
+++ src/pages/Tasks.tsx
@@ -1,13 +1,22 @@
 import { AddTaskModal } from "@/components/modules/tasks/AddTaskModal";
 import TaskCard from "@/components/modules/tasks/TaskCard";
 import { Tabs, TabsList, TabsTrigger } from "@/components/ui/tabs";
+import { useGetTasksQuery } from "@/redux/api/baseApi";
 import { selectTasks, updateFilter } from "@/redux/features/task/taskSlice";
 import { useAppSelector } from "@/redux/hook";
+import type { ITask } from "@/types";
 import { useDispatch } from "react-redux";
 
 const Tasks = () => {
   const tasks = useAppSelector(selectTasks);
-  const dispatch = useDispatch()
+  const dispatch = useDispatch();
+  const {data, isLoading, isError} = useGetTasksQuery(undefined)
+
+  console.log(data, isLoading, isError, tasks);
+
+  if(isLoading){
+    return <p className="text-center">Loading....</p>
+  }
 
   return (
     <div className='mx-auto max-w-7xl px-5 mt-20'>
@@ -24,7 +33,10 @@ const Tasks = () => {
         <AddTaskModal />
       </div>
       <div className='space-y-5 mt-5'>
-        {tasks.map((task, i) => (
+        {/* {tasks.map((task, i) => (
+          <TaskCard key={i + 1} task={task} />
+        ))} */}
+        {!isLoading && data.tasks.map((task: ITask, i: number) => (
           <TaskCard key={i + 1} task={task} />
         ))}
       </div>
